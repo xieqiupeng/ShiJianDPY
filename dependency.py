87792e2e7e9925f021193541d400009bc04a7de2
xieqiupeng <xieqiupeng@gmail.com>
2020-01-06 04:31:59 +0800

    update

--- dependency.py
+++ dependency.py
@@ -3,7 +3,10 @@
 
 # Dependency
 ln -sf /Volumes/Project/Enum/Enum-Idea/意义5问 $Path/2019/4/钱从哪来/意义5问
+ln -sf /Volumes/Project/Enum/Enum-Lesson/不等待 $Path/2016-2017/8/那些男孩教会我的事儿/不等待
+ln -sf /Volumes/Project/Enum/Enum-Lesson/受罪 $Path/2016-2017/8/那些男孩教会我的事儿/受罪
 
 # Root
-Path=/Volumes/Project/Enum/Enum-Subject/时间的朋友
-ln -sf /Volumes/Project/Enum/Enum-Idea/意义5问 $Path/2019/4/钱从哪来/意义5问
+Path=/Volumes/Project/Enum/Enum-Subject/ShiJianDPY
+ln -sf /Volumes/Project/Enum/Enum-Lesson/不等待 $Path/2016-2017/8/那些男孩教会我的事儿/不等待
+ln -sf /Volumes/Project/Enum/Enum-Lesson/受罪 $Path/2016-2017/8/那些男孩教会我的事儿/受罪
